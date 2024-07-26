df5bdb55886b1d5107ccafc49f43aef5c68e85ce
Juani Medone <juan.medone@lambdaclass.com>
2024-07-26 17:09:48 -0300

    Add test_tree_with_3_elements

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -26,6 +26,37 @@ fn test_empty_tree() {
 }
 
 #[test]
+fn test_tree_with_3_elements() {
+    let elements = vec!["a", "b", "c"];
+    let mut tree = MerkleTree::new(elements);
+
+    assert_eq!(tree.leaves.len(), 3);
+    assert_eq!(tree.root.clone().unwrap().len(), 64);
+
+    let mut proof = tree.generate_proof("a").unwrap();
+    assert!(tree.verify("a", proof.clone()));
+    proof.pop();
+    assert!(!tree.verify("a", proof));
+
+    let proof = tree.generate_proof("b").unwrap();
+    assert!(tree.verify("b", proof));
+
+    let proof = tree.generate_proof("c").unwrap();
+    assert!(tree.verify("c", proof));
+
+    assert!(tree.generate_proof("x").is_none());
+
+    tree.add_element("d");
+    assert_eq!(tree.leaves.len(), 4);
+    assert_eq!(tree.root.clone().unwrap().len(), 64);
+
+    let proof = tree.generate_proof("d").unwrap();
+    assert!(tree.verify("d", proof.clone()));
+
+    assert!(!tree.verify("e", proof));
+}
+
+#[test]
 fn test_tree_with_4_elements() {
     let elements = vec!["a", "b", "c", "d"];
     let mut tree = MerkleTree::new(elements);
